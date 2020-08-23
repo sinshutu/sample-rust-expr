1e4f7b4c0b2d750aabfcc39dcbf080d7c4787fe2
NaotoSuzuki <suzuki.z2o.naoto@gmail.com>
2020-08-23 19:39:43 +0900

    単純な加算処理のパーサーを実装する

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let program = String::from("1 + 1 + 1");
+    let v: Vec<i32> = program.split('+').into_iter().map(|s| s.trim().parse().unwrap()).collect();
+    assert_eq!(v, [1, 1, 1]);
+
+    let mut sum: i32 = 0;
+    for n in v.into_iter() {
+        sum += n;
+    }
+
+    println!("{}", sum);
 }
